524b242bedc88d9069782098c8146dd161e9cd21
Sushant <mailsushant@gmail.com>
2013-05-12 15:06:52 -0400

    Move suggest js out of template.

---
+++ js/suggest.js
@@ -0,0 +1,39 @@
+    $(function() {
+      $( "#searchText" ).autocomplete({
+        source: function( request, response ) {
+          $.ajax({
+            url: "http://localhost:8080/suggest",
+            dataType: "jsonp",
+            data: {
+              query: request.term
+            },
+            success: function( data ) {
+              response( $.map( data.result, function( item ) {
+                return {
+                  label: item.name,
+                  value: item.name,
+                  desc: item.tag
+                }
+              }));
+            }
+          });
+        },
+        minLength: 2,
+        /* select: function( event, ui ) { */
+        /*   log( ui.item ? */
+        /*     "Selected: " + ui.item.label : */
+        /*     "Nothing selected, input was " + this.value); */
+        /* }, */
+        open: function() {
+          $( this ).removeClass( "ui-corner-all" ).addClass( "ui-corner-top" );
+        },
+        close: function() {
+          $( this ).removeClass( "ui-corner-top" ).addClass( "ui-corner-all" );
+        }
+      })
+      .data( "ui-autocomplete" )._renderItem = function( ul, item ) {
+        return $( "<li>" )
+          .append( "<a>" + item.label + "<span style=\"float:right;color:#888\">" + item.desc + "</a>" )
+          .appendTo( ul );
+      };
+    });
